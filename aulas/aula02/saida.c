ff3622ae7e5be6db31f0f55a17988cc08aec2059
Rafael Damasceno <rafaeldamasceno300407@gmail.com>
2026-03-11 10:53:28 -0300

    update aula02, added double

--- aulas/aula02/saida.c
+++ aulas/aula02/saida.c
@@ -2,10 +2,10 @@
 #include <math.h>
 
 int main () {
-    int n  = 45.78;
-    printf("o coseno de %d é %f\n",n, cos(n)); //função coseno de um numero inteiro n
-    printf("o seno de %d é %f\n",n, sin(n)); //função seno de um numero inteiro n
-    printf("o tangente de %d é %f\n",n, tan(n)); //função tangente de um numero inteiro n
+    double n  = 467.89;
+    printf("o coseno de %lf é %f\n",n, cos(n)); //função coseno de um numero inteiro n
+    printf("o seno de %lf é %f\n",n, sin(n)); //função seno de um numero inteiro n
+    printf("o tangente de %lf é %f\n",n, tan(n)); //função tangente de um numero inteiro n
 
     return 0;
 
