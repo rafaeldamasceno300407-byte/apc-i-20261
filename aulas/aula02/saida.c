a04ad1333e28f0e4e38f06b7cbb1b0eb43ee4438
Rafael Damasceno <rafaeldamasceno300407@gmail.com>
2026-03-11 10:32:25 -0300

    adicionado aula02

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,12 @@
+#include <stdio.h> //inclusao de bibliotecas da linguagem
+#include <math.h>
+
+int main () {
+    int n  = 45.78;
+    printf("o coseno de %d é %f\n",n, cos(n)); //função coseno de um numero inteiro n
+    printf("o seno de %d é %f\n",n, sin(n)); //função seno de um numero inteiro n
+    printf("o tangente de %d é %f\n",n, tan(n)); //função tangente de um numero inteiro n
+
+    return 0;
+
+}
