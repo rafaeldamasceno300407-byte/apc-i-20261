b89b35e44c3ad4fc0f17bfe77ea23fe77c9e38d7
Rafael Damasceno <rafaeldamasceno300407@gmail.com>
2026-03-25 10:46:00 -0300

    feat

---
+++ praticas/pratica01/ola_mundo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+
+    printf("ola_mundo\n");
+
+    return 0;
+}
---
+++ praticas/pratica02/imprima_caracteres.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+
+    char caractere;
+     printf("informe um caractere: \n");
+     scanf("%c", &caractere);
+     printf("voce precionou o caractere '%c'", caractere);
+
+    return 0;
+}
