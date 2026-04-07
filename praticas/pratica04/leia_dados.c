8ad06dda15ff3589191d8710e9901fb6ad5da074
Rafael Damasceno <rafaeldamasceno300407@gmail.com>
2026-04-07 16:27:42 -0300

    feat: conclui pratica04. Fecha #ID

--- praticas/pratica04/leia_dados.c
+++ praticas/pratica04/leia_dados.c
@@ -4,22 +4,24 @@ int main() {
 
     //Ler a idade, a altura (ex.: `1.75`), o sexo (`M` ou `F`) e o peso (ex.: `72.825` de uma pessoa e exibir todas as informações organizadas em formato de ficha cadastral.
 
-
-    float altura = 1.75; // Variável do tipo ponto flutuante
-    char genero = 'M'; // Variável do tipo caractere
-    float peso = 72.825; // Variável do tipo ponto flutuante
-
+    float altura = 0.0; // Variável do tipo ponto flutuante
+    char genero = ' '; // Variável do tipo caractere
+    float peso = 0.0; // Variável do tipo ponto flutuante
+  
     printf ("Qual a sua altura? ");
      scanf("%f", &altura); 
+         while (getchar() != '\n');
 
     printf ("Qual o seu gênero? ");
      scanf(" %c", &genero); 
-
+         while (getchar() != '\n');
+ 
     printf ("Qual o seu peso? ");
     scanf("%f", &peso); 
-    
+        while (getchar() != '\n');
+
     printf("Altura: %.2f\n", altura);
-    printf("Gênero: %c\n", genero);
+    printf("Genero: %c\n", genero);
     printf("Peso: %.3f\n", peso);
     while (getchar() != '\n');
 
